fe502df41851d21085fb821cc6be84a08a284ffb
Sean Flo <sean@cushmantech.net>
2017-09-13 18:56:28 -0700

    Please fix!

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 </head>
 <body>
 <?php
-	$dir = new DirectoryIterator(__DIR__."/data");
+	$dir = new DirectoryIterator(__DIR__."\data");
 	foreach ($dir as $fileinfo) {
-    if (!$fileinfo->isDot() && @fileinfo->isFile()) {
-        echo "<div class=\"announcement\">".$fileinfo->getPath()."</div>";
+    if (!$fileinfo->isDot() && $fileinfo->isFile()) {
+        echo "<div class=\"announcement\">".$fileinfo->getPathname()."</div>";
     }
 }
 ?>
