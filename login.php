d33674e00f59e90636b0bab2104f1953445be25d
Sean Flo <sean@cushmantech.net>
2017-09-13 22:10:56 -0700

    *facepalm*

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <body>
 <?php if (isSet($error)) echo "<p style=\"color:red\">".$error."</p>"; ?>
 <h1 style="text-align: center;">Please Login:</h1>
-<form style="margin: auto; text-align:center;" method="GET">
+<form style="margin: auto; text-align:center;" method="POST">
 <input type="password" id="accessCode"></input><br/>
 <input type="submit"></input>
 </form>
