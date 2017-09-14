648dbc74ef00cdfeccd69836004b8b30c2ddadd0
Sean Flo <sean@cushmantech.net>
2017-09-13 22:28:24 -0700

    Will it work if I say please?

--- scripts/checkLogin.php
+++ scripts/checkLogin.php
@@ -3,8 +3,9 @@ function checkLogin($password) {
 	$dir = new DirectoryIterator(dirname(__DIR__).DIRECTORY_SEPARATOR."password".DIRECTORY_SEPARATOR);
 	foreach ($dir as $fileinfo) {
 		if (!$fileinfo->isDot() && $fileinfo->isFile()) {	
-			echo $fileinfo->getPathname();
-			if (password_verify($password,file($fileinfo)[0])) {
+			$hash = file($fileinfo);
+			echo $hash[0];
+			if (password_verify($password,$hash[0])) {
 				return true;
 			}
 		}
