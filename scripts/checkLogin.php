62787215ac247a1e686d1ede63026b4caa576ef7
Sean Flo <sean@cushmantech.net>
2017-09-13 22:08:44 -0700

    Fix

--- scripts/checkLogin.php
+++ scripts/checkLogin.php
@@ -3,10 +3,8 @@ function checkLogin(password) {
 	$dir = new DirectoryIterator(__DIR__."\password");
 	foreach ($dir as $fileinfo) {
 		if (!$fileinfo->isDot() && $fileinfo->isFile()) {		
-			if ($fileinfo->getBasename('txt') == file($fileinfo->getPathname())[0]) {
-				if (password_verify(password,$fileinfo->getBasename('txt'))) {
-					return true;
-				}
+			if (password_verify(password,$fileinfo->getBasename('txt'))) {
+				return true;
 			}
 		}
 	}	
