cb60aef53f233e1ab8bda44d9aa1d3c076b533e6
Sean Flo <sean@cushmantech.net>
2017-10-13 09:44:41 -0700

    Debugging on Azure

--- export.php
+++ export.php
@@ -1,5 +1,4 @@
 <?php header('Content-Type: application/xml; charset=utf-8'); ?>
-<?xml version="1.0" encoding="utf-8"?>
 <DeadlineDB xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
   <DeadLineList>
 	<?php
